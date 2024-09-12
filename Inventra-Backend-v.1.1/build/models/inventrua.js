5f047e2bd59af89f4a8a86bbe805fb81b4acb1ae
Kaleb wood <113706839+Shaeferstudios@users.noreply.github.com>
2024-09-12 14:54:00 -0600

    Added user authentication with webtokens
    Everything on our list is done. Users need to logged in to create and update items, since we weren't going to add a delete function in the base app the delete function isn't locked behind authentication.

---
+++ Inventra-Backend-v.1.1/build/models/inventrua.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AssociateUserItem = exports.ItemFactory = exports.Item = void 0;
+const sequelize_1 = require("sequelize");
+const user_1 = require("./user");
+class Item extends sequelize_1.Model {
+}
+exports.Item = Item;
+function ItemFactory(sequelize) {
+    Item.init({ itemId: {
+            type: sequelize_1.DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: sequelize_1.DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: {
+            type: sequelize_1.DataTypes.DATE,
+            defaultValue: sequelize_1.DataTypes.NOW,
+        },
+        updatedAt: {
+            type: sequelize_1.DataTypes.DATE,
+            defaultValue: sequelize_1.DataTypes.NOW,
+        },
+        available: {
+            type: sequelize_1.DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+        userId: {
+            type: sequelize_1.DataTypes.INTEGER,
+            allowNull: true,
+        },
+    }, {
+        tableName: 'Inventory',
+        freezeTableName: true,
+        sequelize
+    });
+}
+exports.ItemFactory = ItemFactory;
+function AssociateUserItem() {
+    user_1.User.hasMany(Item, { foreignKey: 'userId' });
+    Item.belongsTo(user_1.User, { foreignKey: 'userId' });
+}
+exports.AssociateUserItem = AssociateUserItem;
