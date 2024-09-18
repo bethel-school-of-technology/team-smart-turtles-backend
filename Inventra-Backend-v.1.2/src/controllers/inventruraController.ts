e8a5e17183389372887600c023fb7582289c1a18
Kaleb wood <113706839+Shaeferstudios@users.noreply.github.com>
2024-09-18 16:33:53 -0600

    Added CORS compatibility

---
+++ Inventra-Backend-v.1.2/src/controllers/inventruraController.ts
@@ -0,0 +1,88 @@
+import { RequestHandler } from "express";
+import { Item } from "../models/inventrua";
+import { User } from "../models/user";
+import { verifyUser } from "../services/authService";
+
+export const getAllItems: RequestHandler = async (req, res, next) => {
+    let itens = await Item.findAll();
+    res.status(200).json(itens);
+};
+
+export const createItem: RequestHandler = async (req, res, next) => {
+    let user: User | null = await verifyUser(req);
+
+
+    if (!user) {
+        return res.status(403).send();
+    }
+
+    let newMessage: Item = req.body;
+    newMessage.userId = user.userId
+
+    if (newMessage.name && newMessage.available) {
+        let created = await Item.create(newMessage);
+        res.status(201).json(created);
+    } else {
+        res.status(400).send();
+    }
+}
+
+
+export const getItem: RequestHandler = async (req, res, next) => {
+    let itemId = req.params.itemId;
+
+    let itemFound = await Item.findByPk(itemId);
+    if (itemFound) {
+        res.status(200).json(itemFound);
+    }
+    else {
+        res.status(404).json();
+    }
+}
+
+
+
+export const updateItem: RequestHandler = async (req, res, next) => {
+    let user: User | null = await verifyUser(req);
+
+
+    if (!user) {
+        return res.status(403).send();
+    }
+    
+    let itemId = req.params.itemId;
+    let newItem: Item = req.body;
+    newItem.userId = user.userId;
+
+    let messageFound = await Item.findByPk(itemId);
+    try {
+        await Item.update(newItem, {
+            where: { itemId: itemId }
+        });
+        res.status(200).json();
+    } catch {
+        res.status(400).json();
+    }
+
+
+
+}
+
+export const deleteItem: RequestHandler = async (req, res, next) => {
+ 
+   
+    let itemId = req.params.itemId;
+
+
+    let itemFound = await Item.findByPk(itemId);
+
+    if (itemFound) {
+        await Item.destroy({
+            where: { itemId: itemId }
+        });
+        res.status(200).json();
+    }
+    else {
+        res.status(404).json();
+    }
+}
---
+++ Inventra-Backend-v.1.2/src/models/inventrua.ts
@@ -0,0 +1,54 @@
+import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize, TEXT, } from "sequelize";
+import { User } from "./user";
+
+
+export class Item extends Model<InferAttributes<Item>, InferCreationAttributes<Item>>{
+    declare itemId: number;
+    declare name: string;
+    declare createdAt: Date;
+    declare updatedAt: Date;
+    declare available: boolean;
+    declare userId: number;
+    // declare usedBy: string;
+
+}
+
+export function ItemFactory(sequelize: Sequelize) {
+    Item.init({  itemId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      available: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    }, {
+        tableName: 'Inventory',
+        freezeTableName: true,
+        sequelize
+    });
+}
+
+export function AssociateUserItem() {
+  User.hasMany(Item, { foreignKey: 'userId' });
+  Item.belongsTo(User, { foreignKey: 'userId' });
+}
+
+
